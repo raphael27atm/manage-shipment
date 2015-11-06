a40678d02765ae4cdacbde0240db30426fedf0c9
Adriano Vasconcelos <adriano.vasconcelos@secaoweb.com.br>
2015-11-06 18:25:58 +0000

    removendo os configs

--- api/configs/db_config.js
+++
@@ -1,6 +0,0 @@
-var db_string = 'mongodb://localhost/manage-shipment';
-var mongoose = require('mongoose').connect(db_string);
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'Erro ao conectar no banco.'));
